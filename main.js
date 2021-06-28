54f1c7f23460bf05f978ea535b5f60d8df812372
Daniella Dutra <danivetdutra@gmail.com>
2021-06-28 14:32:00 -0300

    Mudança de header da página, com add do scroll

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 // abre e fecha o menu, qdo clicar no icone
 
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-    element.addEventListener('click', function () {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
 // qdo clicar em um item do menu, esconder o menu
 
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
-for (const link of links){
-    link.addEventListener('click', function () {
-        nav.classList.remove ('show')
-    })
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
+// mudar header da página,  qdo der o scroll
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    //scrol é maior que a altura do header
+    header.classList.add("scroll");
+  } else {
+    // menor que a altura do header
+    header.classList.remove("scroll");
+  }
+});
